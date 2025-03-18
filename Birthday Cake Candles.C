b7bca963c02b28036aa1ef0aaf7637fecb87fe0e
ARYAN S <juug24bca16407@jainuniversity.ac.in>
2025-03-18 23:07:25 +0530

    Update Birthday Cake Candles.C

--- Birthday Cake Candles.C
+++ Birthday Cake Candles.C
@@ -1,35 +1,30 @@
-import java.util.Scanner;
+#include <stdio.h>
 
-public class Main {
-    public static int birthdayCakeCandles(int[] candles) {
-        int max = candles[0];
-        int count = 0;
-
-        for (int i = 0; i < candles.length; i++) {
-            if (candles[i] > max) {
-                max = candles[i];
-                count = 1;
-            } else if (candles[i] == max) {
-                count++;
-            }
+int birthdayCakeCandles(int candles[], int n) {
+    int max_height = candles[0];
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (candles[i] > max_height) {
+            max_height = candles[i];
+            count = 1;
+        } else if (candles[i] == max_height) {
+            count++;
         }
-
-        return count;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-        int[] candles = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            candles[i] = scanner.nextInt();
-        }
-
-        int result = birthdayCakeCandles(candles);
-        System.out.println(result);
-
-        scanner.close();
+    return count;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int candles[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &candles[i]);
     }
+
+    int result = birthdayCakeCandles(candles, n);
+    printf("%d\n", result);
+
+    return 0;
 }
